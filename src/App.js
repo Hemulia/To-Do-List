037dd64c9ee8d72741b70c246dd624f3c98b0f64
Emilia Mäkelä <bgr887@myy.haaga-helia.fi>
2022-10-26 02:47:55 +0300

    MUI picker & Tab Menu

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
-import React, {useRef} from 'react';
+
+import React, { useRef, useState } from 'react';
 import './App.css';
 import {AgGridReact} from 'ag-grid-react';
-import 'ag-grid-community/dist/styles/ag-grid.css'
-import 'ag-grid-community/dist/styles/ag-theme-material.css'
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-material.css';
+import Button from'@mui/material/Button';
+import TextField from'@mui/material/TextField';
+import Stack from'@mui/material/Stack';
+import {MuiPickersUtilsProvider, DatePicker} from '@material-ui/pickers';
+import DateFnsUtils from '@date-io/date-fns';
+import Box from '@mui/material/Box';
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
+
+
 
 function App() {
-  const [todo, setTodo] = React.useState({description:'', date:'', priority:''});
+  const [todo, setTodo] = React.useState({description:'', priority:'', selectDate:''});
   const [todos, setTodos] = React.useState([]);
+  const [selectDate, handleDateChange] = useState(new Date());
+  const [value, setValue] = React.useState('1');
   const gridRef = useRef();
 
   const inputChanged = (event) => {
-      setTodo({...todo, [event.target.name]: event.target.value});}
+      setTodo({...todo, [event.target.name]: event.target.value});
+    }
+
 
   const addTodo = (event) => {
       event.preventDefault();
       setTodos([...todos, todo]);
     }
+
+    const handleChange = (event, newValue) => {
+      setValue(newValue);
+    };
 
 
     const deleteTodo = () => {
@@ -26,64 +47,81 @@ function App() {
                 else {
                   alert('Select row first');
                 }}
+   
 
     const columns = [
       {headerName: 'Description', field: 'description', sortable: true, filter: true, floatingFilter: true, animateRows:'true'},
-      {headerName: 'Date', field: 'date', sortable: true, filter:true, floatingFilter: true, animateRows:'true'},
+      {headerName: 'Date', field: 'selectDate', sortable: true, filter:true, floatingFilter: true, animateRows:'true'},
       {headerName: 'Priority', field: 'priority', sortable: true, filter: true, floatingFilter: true, animateRows:'true',
-      cellStyle: params => params.value === "High" ? {color: 'red'} : {color: 'black'}},
-      
+      cellStyle: params => params.value === "High" ? {color: 'red'} : {color: 'black'}},    
     
     ]
-
+  
   return (
-    <div className="App" class="koko">
-      <header>TodoList</header>
-      <form onSubmit={addTodo} onC style={{textAlign:'center', fontSize: 'x-large'}}>
 
-          <input 
-            type= "text" 
+    <div className="App" class="koko">
+       <Box sx={{ width: '100%'}}>
+      <TabContext value={value}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <TabList onChange={handleChange} style ={{marginRight:'45%', float:'right'}}>
+            <Tab label="Home" value="1" />
+            <Tab label="To do list" value="2" />
+          </TabList>
+        </Box>
+        <TabPanel value="1"><h3>Welcome to the Home page</h3>
+        </TabPanel>
+        <TabPanel value="2"><header>TodoList</header>
+      < Stack direction="row" spacing={2}
+       justifyContent="center" alingItems="center" 
+       marginTop="30px" onSubmit={addTodo}>
+          <TextField 
+            label="Description"
             value={todo.description} 
+            variant="standard"
             onChange={inputChanged}
             name="description"
             />
-          <input 
-            type="date" 
-            value={todo.date} 
-            onChange={inputChanged}
-            name="date"
+            <MuiPickersUtilsProvider utils={DateFnsUtils }>
+            <DatePicker 
+            variant="standard"
+            value={selectDate} 
+            onChange={selectDate => handleDateChange(selectDate)}
+            style={{marginTop:'16px'}}
             />
-          <input 
-            type="text" 
+            </MuiPickersUtilsProvider>
+          <TextField 
             value={todo.priority}
+            label="Priority"
+            variant="standard"
             onChange={inputChanged}
             name="priority"
             />
-        <button type="submit" > Add</button>
-        <button type ="button"onClick={deleteTodo}>Delete</button>  
-      </form>
+        <Button type="submit" onClick={addTodo} variant="contained"> Add</Button> 
+        <Button type ="button" onClick={deleteTodo} variant="contained">Delete</Button>
+      </Stack>
+  
       <div
       className="ag-theme-material"
       style={{
         height:'900px',
-        width:'26.5%',
+        width:'25%',
         margin:'auto',
         fontSize: 'x-large',
-        marginLeft:'auto',
-        marginRight:'auto',
-        marginTop:'35px'
+        marginLeft:'1100px',
+        marginRight:'400px'
   
       }}
       >
-        <AgGridReact 
+        <AgGridReact
           ref={gridRef}
-          onGridReady={ params => gridRef.current = params.api }
-          rowSelection="single"
+          onGridReady ={params => gridRef.current = params.api}
+          rowSelection='single'
           columnDefs={columns}
           rowData={todos}>
-          
         </AgGridReact>
-      </div>
+      </div></TabPanel>
+      </TabContext>
+    </Box>
     </div>
   );
 }
